168df3aef64300b152e40dfde7df6f77ec193bc6
Nikolaj Lund Sørensen <nikolajlundsorensen@gmail.com>
2025-01-19 14:53:37 +0100

    Enable cors on both APIs and add swagger ui to Docker compose

---
+++ src/VAF13.Klubadmin.API/Infrastructure/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Options;
+using VAF13.Klubadmin.Domain.Configurations;
+
+namespace VAF13.Klubadmin.API.Infrastructure.Middleware;
+
+public class ApiKeyMiddleware
+{
+  private readonly RequestDelegate _requestDelegate;
+  private readonly ApiConfiguration _apiConfiguration;
+  private const string ApiKey = "X-API-KEY";
+
+  public ApiKeyMiddleware(RequestDelegate requestDelegate, IOptions<ApiConfiguration> options)
+  {
+    _requestDelegate = requestDelegate;
+    _apiConfiguration = options.Value;
+    if (string.IsNullOrEmpty(_apiConfiguration.APIKey))
+    {
+      throw new ArgumentException("API Configuration for API key is empty!");
+    }
+  }
+
+  public async Task Invoke(HttpContext context)
+  {
+    if (!context.Request.Headers.TryGetValue(ApiKey, out var apiKeyVal))
+    {
+      context.Response.StatusCode = 401;
+      await context.Response.WriteAsync("Api Key not found!");
+      return;
+    }
+
+    if (!_apiConfiguration.APIKey.Equals(apiKeyVal))
+    {
+      context.Response.StatusCode = 401;
+      await context.Response.WriteAsync("Unauthorized client");
+      return;
+    }
+
+    await _requestDelegate(context);
+  }
+}
--- src/VAF13.Klubadmin.Domain/Configurations/ApiConfiguration.cs
+++ src/VAF13.Klubadmin.Domain/Configurations/ApiConfiguration.cs
@@ -4,4 +4,6 @@ public class ApiConfiguration
 {
   public const string ConfigurationSectionName = "ApiConfiguration";
   public string APIKey { get; set; } = string.Empty;
+  public string CorsOrigins { get; set; } = string.Empty;
+  public bool AddCors => !string.IsNullOrEmpty(CorsOrigins);
 }
